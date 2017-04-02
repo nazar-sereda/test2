b0cc720dadb4c052e7a0ec7e005705e74564dcd1
nazar-sereda <nazar.y.sereda@gmail.com>
2017-04-02 18:17:17 +0300

    JS

---
+++ reverse_array.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Nazar on 02.04.2017.
+ */
+
+var names = ["Nazar", "Taras", "Yana"];
+
+for (j=0; j<names.length; j++){
+    console.log(names[j]);
+}
+names.reverse();
+for (i=0; i<names.length; i++ ){
+    console.log(names[i]);
+}
+
